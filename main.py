3c6a11566b9c915c318574c8b4c38684834a7fbc
Galileo4446 <galileo4446@gmail.com>
2020-06-11 16:42:47 +0900

    bsoを正しく表示できるよう変更

--- main.py
+++ main.py
@@ -1,43 +1,74 @@
 from urllib import request  # urllib.requestモジュールをインポート
 from bs4 import BeautifulSoup  # BeautifulSoupクラスをインポート
+import time
 
-print('速報できる試合を調べています。')
+# print('速報できる試合を調べています。')
 
 # チーム名入力
 teams={'top': 'バッファローズ', 'bottom': 'ホークス'}
 
 print(teams['top'] + '対' + teams['bottom'] + 'の試合経過をお伝えします。')
 
-
-
 def main():
-    url = 'https://baseball.yahoo.co.jp/npb_practice/game/2020061016/score?index=0520300'
-    response = request.urlopen(url)
-    soup = BeautifulSoup(response, features="html.parser")
-    response.close()
+    inning=''
+    score=''
+    bso={'ball': -1, 'strike': -1, 'out': -1}
+    batter=''
+    pitcher=''
+    result=''
+    while True:
+        url = 'https://baseball.yahoo.co.jp/npb_practice/game/2020061116/score'
+        response = request.urlopen(url)
+        soup = BeautifulSoup(response, features="html.parser")
+        response.close()
 
-    inning=soup.select('.live em')[0].text
-    score=score_converter(soup.select('.score table td'))
-    bso=bso_converter(soup.select('.sbo b'))
+        if inning!=soup.select('.live em')[0].text:
+            inning=soup.select('.live em')[0].text
+            print(inning)
 
-    batter=get_batter_name(soup)
-    pitcher=get_pitcher_name(soup)
+        if score!=score_converter(soup.select('.score table td')) or inning!=soup.select('.live em')[0].text:
+            score=score_converter(soup.select('.score table td'))
+            print(score_message(score))
 
-    print(inning)
-    print(score_message(score))
-    # TODO ランナー文字変換関数作る
-    runner='なし'
-    # print(bso['out'] + 'アウト、ランナー' + runner)
-    print(pitcher)
-    print(batter)
-    print(bso_message(bso))
-    # print('カウントは、' + bso['ball'] + 'ボール' + bso['strike'] + 'ストライク')
+        if batter!=get_batter_name(soup):
+            batter=get_batter_name(soup)
+            print(batter)
 
-    result=soup.select('[class=bb-splits__item] table')[2].select('tbody')[0].select('tr')[-1].select('.bb-splitsTable__data')
-    batting_result=get_batting_result(result)
-    print(batting_result_message(batting_result))
-    # 点差に応じて得点コメントする。
-    # 継投 代打 守備 代走など対応。その時は打者空白になる。
+        if pitcher!=get_pitcher_name(soup):
+            pitcher=get_pitcher_name(soup)
+            print(pitcher)
+
+        # TODO ランナー文字変換関数作る
+        runner='なし'
+        # print(bso['out'] + 'アウト、ランナー' + runner)
+        # 打席中（打者名が変わっていない）ならボールカウント
+        # アウトカウント変わったらアウトカウント
+
+        try:
+            if result!=soup.select('[class=bb-splits__item] table')[2].select('tbody')[0].select('tr')[0].select('.bb-splitsTable__data'):
+                result=soup.select('[class=bb-splits__item] table')[2].select('tbody')[0].select('tr')[0].select('.bb-splitsTable__data')
+                batting_result=get_batting_result(result)
+                print(batting_result_message(batting_result))
+        except:
+            pass
+            # print('次のバッターを待機しています。')
+        
+        bso_archive=bso
+        if bso_archive!=bso_converter(soup.select('.sbo b')):
+            bso=bso_converter(soup.select('.sbo b'))
+            if bso_archive['out']==bso['out']:
+                print(bso_message(bso))
+            else:
+                print(out_count_message(bso['out']))
+
+                
+                
+
+        
+        # 点差に応じて得点コメントする。
+        # 継投 代打 守備 代走など対応。その時は打者空白になる。
+        # print('10秒ごとに更新します')
+        time.sleep(10.0)
 
 
 # def get_match_data():
@@ -46,10 +77,23 @@ def main():
 
 # 関数呼び出しはbsoが3まであることを確認してから！様々な処理に使うから大事。
 def bso_converter(bso):
-    return {'ball': len(bso[0]), 'strike': len(bso[1]), 'out': len(bso[2])}
+    return {'ball': len(bso[0].text), 'strike': len(bso[1].text), 'out': len(bso[2].text)}
 
 def bso_message(bso):
-    return 'カウントは' + count_name(bso['ball']) + 'ボール' + count_name(bso['strike']) + 'ストライク' if len(count_name(bso['ball'])) * len(count_name(bso['strike'])) else ''
+    if bso['strike']==0 and bso['ball']==0:
+        return ''
+    elif bso['strike']==3 or bso['ball']==4:
+        return ''
+    else:
+        return count_name(bso['ball']) + 'ボール' + count_name(bso['strike']) + 'ストライク'
+
+def out_count_message(out_count):
+    if 0<out_count<3:
+        return count_name(out_count) + 'アウトです。'
+    elif out_count==3:
+        return count_name(out_count) + 'アウトチェンジです。'
+    else:
+        return ''
 
 def count_name(num):
     if num == 0:
@@ -91,10 +135,6 @@ def get_batter_name(soup):
 def get_batting_result(result):
     return{'number' : result[0].text.strip(), 'total' : result[1].text.strip(), 'type' : result[2].text.strip(), 'speed' : result[3].text.strip(), 'result' : result[4].text.replace(' ', '').replace('\n', '')}
 
-# 打席継続 or 打席終了 or イニング終了のラベルつける？ アウトカウントで判断すればいらないかも。バッター名毎回取得するのは大変でもない。ただ変更はどうにか検知？
-# ①継続系の結果を取り出す。メッセージ作成
-# ②打球飛ばない系の結果取り出す。メッセージ作成。
-# ③打球が飛んだ結果の解析。後半で結果を判定。最初の文字でポジション判別。右中間左中間に注意。見逃している結果もあるかもしれないから注意。
 def batting_result_message(result):
     result_name=result['result'].split('[')[0]
     speed=result['speed'].split('km/h')[0] + 'キロ'
@@ -106,15 +146,15 @@ def batting_result_message(result):
     elif result_name=='空振り':
         return count + 'を空振り。' + result['type'] + 'でした。'
     elif result_name=='ファウル':
-        return count + 'は、ファウルボール。'
+        return count + '、' + result['type'] + 'を打って、ファウルボール。'
     elif result_name=='四球':
-        return count + '見送ってフォアボール'
+        return count + '、見送ってフォアボール'
     elif result_name=='死球':
         return 'おっと、これはデッドボールとなってしまいました。'
     elif result_name=='見三振':
         return count + '、入りました！見逃し三振！' + '最後は' + speed + 'の' + result['type'] + 'でした。'
     elif result_name=='空三振':
-        return count + '、空振り三振！' + '最後は' + speed + 'の' + result['type'] + '。決まっています。'
+        return count + '、空振り三振！' + '最後は' + speed + 'の' + result['type'] + 'が決まりました。'
     elif result_name.endswith('安'):
         return count + position_name_converter(result_name.split('安')[0]) + 'へのヒットになりました。打ったのは' + result['type'] + 'でしょうか。'
     elif result_name.endswith(('２', '2')):
